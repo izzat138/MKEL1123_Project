de24d670613d142378a1eb7a55262e40a7b82e31
izzat138 <106621749+izzat138@users.noreply.github.com>
2023-02-11 03:13:23 +0800

    Delete STM32_Speech_To_Sign_Language_Translator/Debug directory

--- STM32_Speech_To_Sign_Language_Translator/Debug/Core/Src/execution_time.d
+++
@@ -1,3 +0,0 @@
-Core/Src/execution_time.o: ../Core/Src/execution_time.c \
- C:/Users/idris/Downloads/stm32-speech-recognition-and-traduction-master/Core/Inc/execution_time.h
-C:/Users/idris/Downloads/stm32-speech-recognition-and-traduction-master/Core/Inc/execution_time.h:
